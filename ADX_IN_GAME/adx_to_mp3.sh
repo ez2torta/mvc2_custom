0d607b659cb655fb6382b15749e203c2cd94ea96
Paulo Sandoval <ez2torta@gmail.com>
2024-04-21 22:57:25 -0400

    script to convert ADX to MP3

---
+++ ADX_IN_GAME/adx_to_mp3.sh
@@ -0,0 +1,87 @@
+radx_decode ADX_CAPL.ADX
+radx_decode ADX_CONT.ADX
+radx_decode ADX_HERE.ADX
+radx_decode ADX_MENU.ADX
+radx_decode ADX_OPEN.ADX
+radx_decode ADX_OVER.ADX
+radx_decode ADX_RANK.ADX
+radx_decode ADX_S000.ADX
+radx_decode ADX_S010.ADX
+radx_decode ADX_S020.ADX
+radx_decode ADX_S030.ADX
+radx_decode ADX_S040.ADX
+radx_decode ADX_S050.ADX
+radx_decode ADX_S060.ADX
+radx_decode ADX_S070.ADX
+radx_decode ADX_S080.ADX
+radx_decode ADX_S090.ADX
+radx_decode ADX_S0A0.ADX
+radx_decode ADX_S0B0.ADX
+radx_decode ADX_S0C0.ADX
+radx_decode ADX_S0D0.ADX
+radx_decode ADX_S0E0.ADX
+radx_decode ADX_S0F0.ADX
+radx_decode ADX_S100.ADX
+radx_decode ADX_S110.ADX
+radx_decode ADX_S120.ADX
+radx_decode ADX_S130.ADX
+radx_decode ADX_S140.ADX
+radx_decode ADX_S150.ADX
+radx_decode ADX_S160.ADX
+radx_decode ADX_S170.ADX
+radx_decode ADX_S180.ADX
+radx_decode ADX_S190.ADX
+radx_decode ADX_S1A0.ADX
+radx_decode ADX_S1B0.ADX
+radx_decode ADX_S1C0.ADX
+radx_decode ADX_S1D0.ADX
+radx_decode ADX_S1E0.ADX
+radx_decode ADX_S1F0.ADX
+radx_decode ADX_SELC.ADX
+radx_decode ADX_STAF.ADX
+radx_decode ADX_WINS.ADX
+
+ffmpeg -i ADX_CAPL.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_CAPL.mp3 
+ffmpeg -i ADX_CONT.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_CONT.mp3 
+ffmpeg -i ADX_HERE.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_HERE.mp3 
+ffmpeg -i ADX_MENU.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_MENU.mp3 
+ffmpeg -i ADX_OPEN.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_OPEN.mp3 
+ffmpeg -i ADX_OVER.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_OVER.mp3 
+ffmpeg -i ADX_RANK.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_RANK.mp3 
+ffmpeg -i ADX_S000.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S000.mp3 
+ffmpeg -i ADX_S010.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S010.mp3 
+ffmpeg -i ADX_S020.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S020.mp3 
+ffmpeg -i ADX_S030.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S030.mp3 
+ffmpeg -i ADX_S040.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S040.mp3 
+ffmpeg -i ADX_S050.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S050.mp3 
+ffmpeg -i ADX_S060.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S060.mp3 
+ffmpeg -i ADX_S070.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S070.mp3 
+ffmpeg -i ADX_S080.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S080.mp3 
+ffmpeg -i ADX_S090.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S090.mp3 
+ffmpeg -i ADX_S0A0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0A0.mp3 
+ffmpeg -i ADX_S0B0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0B0.mp3 
+ffmpeg -i ADX_S0C0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0C0.mp3 
+ffmpeg -i ADX_S0D0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0D0.mp3 
+ffmpeg -i ADX_S0E0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0E0.mp3 
+ffmpeg -i ADX_S0F0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S0F0.mp3 
+ffmpeg -i ADX_S100.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S100.mp3 
+ffmpeg -i ADX_S110.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S110.mp3 
+ffmpeg -i ADX_S120.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S120.mp3 
+ffmpeg -i ADX_S130.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S130.mp3 
+ffmpeg -i ADX_S140.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S140.mp3 
+ffmpeg -i ADX_S150.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S150.mp3 
+ffmpeg -i ADX_S160.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S160.mp3 
+ffmpeg -i ADX_S170.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S170.mp3 
+ffmpeg -i ADX_S180.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S180.mp3 
+ffmpeg -i ADX_S190.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S190.mp3 
+ffmpeg -i ADX_S1A0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1A0.mp3 
+ffmpeg -i ADX_S1B0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1B0.mp3 
+ffmpeg -i ADX_S1C0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1C0.mp3 
+ffmpeg -i ADX_S1D0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1D0.mp3 
+ffmpeg -i ADX_S1E0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1E0.mp3 
+ffmpeg -i ADX_S1F0.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_S1F0.mp3 
+ffmpeg -i ADX_SELC.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_SELC.mp3 
+ffmpeg -i ADX_STAF.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_STAF.mp3 
+ffmpeg -i ADX_WINS.ADX.wav -vn -ar 44100 -ac 2 -b:a 192k ADX_WINS.mp3 
+
+rm *.ADX.wav
